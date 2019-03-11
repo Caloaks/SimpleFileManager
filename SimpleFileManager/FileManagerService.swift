9b96b215ba465cee736c42cdfe6fc2ff20949d53
Loriens <lerd.95@mail.ru>
2019-03-11 12:53:53 +0300

    Add FileManagerService

---
+++ SimpleFileManager/FileManagerService.swift
@@ -0,0 +1,9 @@
+//
+//  FileManagerService.swift
+//  SimpleFileManager
+//
+//  Created by Vladislav on 11/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import Foundation
