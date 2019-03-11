3f0a53c79d20de193d189353e108c5c84cd930f6
Loriens <lerd.95@mail.ru>
2019-03-11 12:04:16 +0300

    Initial Commit

---
+++ SimpleFileManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleFileManager
+//
+//  Created by Vladislav on 11/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
